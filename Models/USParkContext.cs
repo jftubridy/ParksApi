6fe79c3ee3305f3cde14efbba9f48a3d0b41d6c5
Joseph Tubridy <jftubridy@gmail.com>
2019-08-23 13:53:36 -0700

    had a problem with some syntax that was hindering postman

--- Models/USParkContext.cs
+++ Models/USParkContext.cs
@@ -7,6 +7,7 @@ namespace USPark.Models
 
         public DbSet<Park> Parks { get; set; }
 
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder
                 .UseMySql(@"server=localhost;user id=root;password=epicodus;port=3306;database=us_parks;");

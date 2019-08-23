5587d30f15d8d2830e079dc4727593eae90d24c4
Joseph Tubridy <jftubridy@gmail.com>
2019-08-23 15:29:47 -0700

    reverted to a previous commit after swagger set things off, weill try and remember to use a branch

--- Startup.cs
+++ Startup.cs
@@ -40,6 +40,7 @@ namespace USPark
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            //was adding swagger but had to revert
 
             // app.UseHttpsRedirection();
             app.UseMvc();
